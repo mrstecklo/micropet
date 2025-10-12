77e14336e1f15f076f40debc824e16904c5bb876
mrstecklo <mrstecklo@yandex.ru>
2025-10-12 20:34:47 +0300

    Forward orders requests

--- services/api-gateway/http_handler.go
+++ services/api-gateway/http_handler.go
@@ -25,21 +25,24 @@ type httpHandlerMuxConfig struct {
 
 func newHttpHandlerMux(config httpHandlerMuxConfig) httpHandlerMux {
 	mux := http.NewServeMux()
-	mux.Handle("/orders", httpHandler{config.logger, handleOrders})
+	mux.Handle("/orders", httpHandler{config.logger, handleOrders, config.orders})
 	return httpHandlerMux{mux}
 }
 
-type handleFunc func(*slog.Logger, http.ResponseWriter, *http.Request)
+type handleFunc func(*slog.Logger, server, http.ResponseWriter, *http.Request)
 
 type httpHandler struct {
 	logger *slog.Logger
 	handle handleFunc
+	server server
 }
 
 func (h httpHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-	h.handle(h.logger, responseWriter, request)
+	h.handle(h.logger, h.server, responseWriter, request)
 }
 
-func handleOrders(logger *slog.Logger, responseWriter http.ResponseWriter, request *http.Request) {
-	responseWriter.WriteHeader(http.StatusMethodNotAllowed)
+func handleOrders(logger *slog.Logger, server server, responseWriter http.ResponseWriter, request *http.Request) {
+	req, _ := http.NewRequest("GET", server.url, nil)
+	resp, _ := server.client.Do(req)
+	responseWriter.WriteHeader(resp.StatusCode)
 }
--- services/api-gateway/http_handler_test.go
+++ services/api-gateway/http_handler_test.go
@@ -6,22 +6,59 @@ import (
 	"testing"
 )
 
-func TestHttpHandler(t *testing.T) {
+type httpHandlerFixtureConfig struct {
+	ordersServerHandler func(http.ResponseWriter, *http.Request)
+}
+
+type httpHandlerFixture struct {
+	handler          httpHandlerMux
+	responseRecorder *httptest.ResponseRecorder
+	ordersServer     *httptest.Server
+}
+
+func setUpHttpHandler(t *testing.T, config httpHandlerFixtureConfig) httpHandlerFixture {
 	logger := createLogger()
-	request := httptest.NewRequest("GET", "/orders", nil)
-	responseRecorder := httptest.NewRecorder()
-	ordersServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}))
-	defer ordersServer.Close()
+	ordersServer := httptest.NewServer(http.HandlerFunc(config.ordersServerHandler))
 	handler := newHttpHandlerMux(httpHandlerMuxConfig{
 		logger: logger,
 		orders: server{ordersServer.URL, ordersServer.Client()},
 	})
+	t.Cleanup(ordersServer.Close)
+	return httpHandlerFixture{
+		handler:          handler,
+		responseRecorder: httptest.NewRecorder(),
+		ordersServer:     ordersServer,
+	}
+}
 
-	handler.ServeHTTP(responseRecorder, request)
+func TestHttpHandler_GetOrdersNotAllowed(t *testing.T) {
+	config := httpHandlerFixtureConfig{
+		ordersServerHandler: func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		},
+	}
+	f := setUpHttpHandler(t, config)
+	request := httptest.NewRequest("GET", "/orders", nil)
 
-	if http.StatusMethodNotAllowed != responseRecorder.Code {
-		t.Errorf("incorrect code: expected %d, got %d", http.StatusMethodNotAllowed, responseRecorder.Code)
+	f.handler.ServeHTTP(f.responseRecorder, request)
+
+	if http.StatusMethodNotAllowed != f.responseRecorder.Code {
+		t.Errorf("incorrect code: expected %d, got %d", http.StatusMethodNotAllowed, f.responseRecorder.Code)
+	}
+}
+
+func TestHttpHandler_GetOrdersInternalError(t *testing.T) {
+	config := httpHandlerFixtureConfig{
+		ordersServerHandler: func(w http.ResponseWriter, r *http.Request) {
+			http.Error(w, "Method not allowed", http.StatusInternalServerError)
+		},
+	}
+	f := setUpHttpHandler(t, config)
+	request := httptest.NewRequest("GET", "/orders", nil)
+
+	f.handler.ServeHTTP(f.responseRecorder, request)
+
+	if http.StatusInternalServerError != f.responseRecorder.Code {
+		t.Errorf("incorrect code: expected %d, got %d", http.StatusInternalServerError, f.responseRecorder.Code)
 	}
 }
